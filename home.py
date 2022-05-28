f07595189adddac5c6783f427f1fb4fd05a14469
Khushi Sharma <84229879+khushi10000@users.noreply.github.com>
2022-05-28 15:51:30 +0530

    Update home.py

--- home.py
+++ home.py
@@ -4,8 +4,6 @@ import quickRecommend
 import TopMovies
 from multiapp import MultiApp
 import sys
-if sys.version_info[3:9] != (3, 9):
-    raise Exception('Requires python 3.9')
 app = MultiApp()
 # st.set_page_config(page_title=’TrekViz’, page_icon=”🖖”)
 app.add_app("Home Page", TitlePage.app)
